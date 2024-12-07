580d90d9997f990f73d1df8dd55c7160664c6758
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-12-06 22:50:09 -0500

    FEAT: Add locale bean for internationalization

---
+++ src/main/java/xyz/cupscoffee/hackathondwi/shared/adapter/in/view/LocaleBean.java
@@ -0,0 +1,37 @@
+package xyz.cupscoffee.hackathondwi.shared.adapter.in.view;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.LocaleResolver;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@Named
+@SessionScoped
+@RequiredArgsConstructor
+public class LocaleBean {
+    private final LocaleResolver localeResolver;
+    private final HttpServletRequest request;
+    private final HttpServletResponse response;
+
+    public String getLanguage() {
+        return localeResolver.resolveLocale(request).getLanguage();
+    }
+
+    public void changeLanguage(String language) {
+        Locale locale = Locale.forLanguageTag(language);
+        localeResolver.setLocale(request, response, locale);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+
+        log.debug("Language changed to: {}", language);
+    }
+}
