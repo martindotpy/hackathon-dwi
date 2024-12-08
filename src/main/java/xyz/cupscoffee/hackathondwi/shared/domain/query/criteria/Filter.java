e7bfd98306ef05d9b0877132518169de030de96f
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-12-07 22:58:26 -0500

    FEAT: Add messages

--- src/main/java/xyz/cupscoffee/hackathondwi/shared/domain/query/criteria/Filter.java
+++ src/main/java/xyz/cupscoffee/hackathondwi/shared/domain/query/criteria/Filter.java
@@ -14,4 +14,64 @@ public final class Filter {
     private final String field;
     private final FilterOperator operator;
     private final String value;
+
+    /**
+     * Create a filter with a string value.
+     *
+     * @param field    The field to filter.
+     * @param operator The operator to use.
+     * @param value    The value to filter.
+     * @return The filter
+     */
+    public static Filter of(String field, FilterOperator operator, String value) {
+        return new Filter(field, operator, value);
+    }
+
+    /**
+     * Create a filter with a long value.
+     *
+     * @param field    The field to filter.
+     * @param operator The operator to use.
+     * @param value    The value to filter.
+     * @return The filter
+     */
+    public static Filter of(String field, FilterOperator operator, Long value) {
+        return new Filter(field, operator, String.valueOf(value));
+    }
+
+    /**
+     * Create a filter with an integer value.
+     *
+     * @param field    The field to filter.
+     * @param operator The operator to use.
+     * @param value    The value to filter.
+     * @return
+     */
+    public static Filter of(String field, FilterOperator operator, Integer value) {
+        return new Filter(field, operator, String.valueOf(value));
+    }
+
+    /**
+     * Create a filter with a double value.
+     *
+     * @param field    The field to filter.
+     * @param operator The operator to use.
+     * @param value    The value to filter.
+     * @return
+     */
+    public static Filter of(String field, FilterOperator operator, Double value) {
+        return new Filter(field, operator, String.valueOf(value));
+    }
+
+    /**
+     * Create a filter with a boolean value.
+     *
+     * @param field    The field to filter.
+     * @param operator The operator to use.
+     * @param value    The value to filter.
+     * @return The filter
+     */
+    public static Filter of(String field, FilterOperator operator, Boolean value) {
+        return new Filter(field, operator, String.valueOf(value));
+    }
 }
